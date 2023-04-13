655eaa8b084448e1ca6f82b6a9741382c6eed827
Nvtsaruk <75068406+Nvtsaruk@users.noreply.github.com>
2023-04-13 13:04:22 +0300

    modified:   Script/script.js 	modified:   css/style.css 	modified:   index.html

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,7 @@
-window.addEventListener('resize', () => {
-    if(window.innerWidth <= 375) {
-        document.getElementById('mainImg').src = "assets/images/image-web-3-mobile.jpg"
-    } else {
-        document.getElementById('mainImg').src = "assets/images/image-web-3-desktop.jpg"
-    }
-})
+// window.addEventListener('resize', () => {
+//     if(window.innerWidth <= 375) {
+//         document.getElementById('mainImg').src = "assets/images/image-web-3-mobile.jpg"
+//     } else {
+//         document.getElementById('mainImg').src = "assets/images/image-web-3-desktop.jpg"
+//     }
+// })
